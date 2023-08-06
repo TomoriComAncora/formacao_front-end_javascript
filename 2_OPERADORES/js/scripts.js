4f8c68567df6fbfb545cd9614d0af5061e4bb2b1
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-06 18:57:25 -0400

    basico_js

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,31 @@
+// 1 Number
+console.log(typeof 2);
+console.log(typeof 2.453);
+console.log(typeof -29);
+
+// 2 opr. aritimético
+console.log(7 + 17 + 4 - 8);
+console.log(3 + 2 * (8 / 3));
+
+// 3 special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(typeof 3/0);
+console.log(typeof 34 * "fsdf");
+console.log(typeof NaN);
+
+// 4 string
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log('+ 1 outro texto');
+console.log("11");
+
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`);
+console.log(typeof '+ 1 outro texto');
+
+// 5 caracteres especiais
+console.log("Testando a \nQuebra de linha");
+console.log("Espaçamento de \t tab");
