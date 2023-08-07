cb3c54171f21b330aa861a13830e6915ac19d769
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-07 12:36:04 -0400

    fim_estudo_trabalho_js

---
+++ 3_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,50 @@
+// 1 variáveis
+let nome = "Lucas";
+
+console.log(nome);
+
+nome = "Lucas Fernando";
+
+console.log(nome);
+
+const idade = 21;
+
+console.log(idade);
+
+// idade = 22;
+
+// 2 mais sobre variáveis
+// let 2teste = "inválido";
+// let 2teste = "inválido"
+
+let a = 10,
+  b = 20,
+  c = 30;
+
+console.log(a, b, c);
+
+let nomeCompleto = "Lucas Fernando";
+let nomecompleto = "lucas fernando";
+
+console.log(nomeCompleto);
+console.log(nomecompleto);
+
+// 3 função prompt
+// const age = prompt("Digite sua idade!:");
+// console.log(`A sua idade é ${age} anos!`);
+
+// 4 função alert
+// alert("testando");
+
+// const teste = 11;
+// alert(`Número é ${teste}`);
+
+// 5 objeto Math
+console.log(Math.max(1, 5, 6, 10));
+console.log(Math.floor(8.23));
+console.log(Math.ceil(7.59));
+
+// 6 objeto console
+console.log("Exibe mensagem");
+console.error("Erro!");
+console.warn("Aviso!");
