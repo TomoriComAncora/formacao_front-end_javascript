c1bc8c32200c57eb3cdc3f08afd4239b0202848a
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-09 20:08:01 -0400

    inicio_arrays

---
+++ 5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -0,0 +1,22 @@
+// 1 arrays
+const lista = [1, 2, 3, 4, 5];
+
+console.log(lista);
+console.log(typeof lista);
+
+const itens = ["Lucas", 1, 2.44, lista, []];
+console.log(itens);
+
+// 2 mais sobre arrays
+
+const letras = ["a", "b", "c", "d"];
+console.log(letras[0]);
+console.log(letras[1]);
+console.log(letras[2]);
+
+// 3 Porpriedades
+const nums = [1, 2, 3, 4];
+console.log(nums.length);
+
+const myName = "Lucas";
+console.log(myName.length);
