358464436060701299866bac08efb178ff522c1a
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-22 12:50:44 -0400

    mias_um_evento

--- 10_EVENTOS/js/scripts.js
+++ 10_EVENTOS/js/scripts.js
@@ -31,3 +31,82 @@ myTitle.addEventListener("click", (e) => {
   console.log(e.pointerType);
   console.log(e.target);
 });
+
+// 4 propagação
+const divbtn = document.querySelector("#btn-container");
+const insiderBtn = document.querySelector("#div-btn");
+
+divbtn.addEventListener("click", () => {
+  console.log("Evento 1");
+});
+
+insiderBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("Evento 2");
+});
+
+// 5 Evento padrão
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Não alterou a página");
+});
+
+// 6 Evento de Tecla
+document.addEventListener("keyup", (e) => {
+  console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(`Apertou a tecla ${e.key}`);
+});
+
+// 7 Outros eventos de mouse
+const mouse = document.querySelector("#mouse");
+
+mouse.addEventListener("mousedown", (e) => {
+  console.log(`Apertou o botão`);
+});
+
+mouse.addEventListener("mouseup", (e) => {
+  console.log(`soltou o botão`);
+});
+
+mouse.addEventListener("dblclick", (e) => {
+  console.log(`Click duplo`);
+});
+
+// 8 movimento do mouse
+document.addEventListener("mousemove", (e) => {
+  // console.log(`No eixo X:${e.x}`);
+  // console.log(`No eixo Y:${e.y}`);
+});
+
+// 9 Evento com scroll
+window.addEventListener("scroll", (e) => {
+  if (window.pageYOffset > 200) {
+    console.log("Passamos de 200px");
+  }
+});
+
+// 10 evento focus/blur
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", () => {
+  console.log("Está em foco");
+});
+
+input.addEventListener("blur", () => {
+  console.log("Não está em foco");
+});
+
+// 11 Evento de carregamento
+window.addEventListener("load", () => {
+  console.log("A página carregou!");
+});
+
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  e.returnValue = "teste";
+});
